e8196b347f3f9465c2874563ecdfc6b474a93b91
samuelteodoro <samuelteodoroferreira@gmail.com>
2023-09-18 14:37:20 -0300

    Ataualizando os menus

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,12 +6,132 @@ function showFrame(frameId) {
     });
 
     // Exibe o iframe especificado
-    const frameToShow = document.getElementById(frameId);
-    if (frameToShow) {
-        frameToShow.style.display = 'block';
-    }
-}
+            const frameToShow = document.getElementById(frameId);
+            if (frameToShow) {
+                frameToShow.style.display = 'block';
+            }
+        }
 
-window.onload = function () {
-    alert("A página foi carregada.");
-};
+        //Definindo o tamanho do Iframe Cedro
+        window.addEventListener('DOMContentLoaded', (event) => {
+            // Obtenha a referência para o elemento <iframe>
+            const iframe = document.getElementById('cedro-frame');
+               
+            // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+            iframe.onload = function() {
+                const conteudo = iframe.contentDocument.documentElement;
+                const alturaConteudo = conteudo.scrollHeight;
+        
+                // Defina a altura do <iframe> para a altura do conteúdo
+                iframe.style.height = alturaConteudo + 'px';
+            };
+        });
+
+        //Definindo o tamanho do iperosa-frame
+        window.addEventListener('DOMContentLoaded', (event) => {
+            // Obtenha a referência para o elemento <iframe>
+            const iframe = document.getElementById('iperosa-frame');
+            
+            // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+            iframe.onload = function() {
+                const conteudo = iframe.contentDocument.documentElement;
+                const alturaConteudo = conteudo.scrollHeight;
+
+                // Defina a altura do <iframe> para a altura do conteúdo
+                iframe.style.height = alturaConteudo + 'px';
+            };
+        });
+
+           //Definindo o tamanho do oiti-frame
+           window.addEventListener('DOMContentLoaded', (event) => {
+            // Obtenha a referência para o elemento <iframe>
+            const iframe = document.getElementById('oiti-frame');
+            
+            // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+            iframe.onload = function() {
+                const conteudo = iframe.contentDocument.documentElement;
+                const alturaConteudo = conteudo.scrollHeight;
+
+                // Defina a altura do <iframe> para a altura do conteúdo
+                iframe.style.height = alturaConteudo + 'px';
+            };
+        });
+
+  //Definindo o tamanho do paineira-frame
+  window.addEventListener('DOMContentLoaded', (event) => {
+    // Obtenha a referência para o elemento <iframe>
+    const iframe = document.getElementById('paineira-frame');
+    
+    // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+    iframe.onload = function() {
+        const conteudo = iframe.contentDocument.documentElement;
+        const alturaConteudo = conteudo.scrollHeight;
+
+        // Defina a altura do <iframe> para a altura do conteúdo
+        iframe.style.height = alturaConteudo + 'px';
+    };
+});
+
+ //Definindo o tamanho do paineira-frame
+ window.addEventListener('DOMContentLoaded', (event) => {
+    // Obtenha a referência para o elemento <iframe>
+    const iframe = document.getElementById('paubrasil-frame');
+    
+    // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+    iframe.onload = function() {
+        const conteudo = iframe.contentDocument.documentElement;
+        const alturaConteudo = conteudo.scrollHeight;
+
+        // Defina a altura do <iframe> para a altura do conteúdo
+        iframe.style.height = alturaConteudo + 'px';
+    };
+});
+
+ //Definindo o tamanho do paineira-frame
+ window.addEventListener('DOMContentLoaded', (event) => {
+    // Obtenha a referência para o elemento <iframe>
+    const iframe = document.getElementById('pracaamaral-frame');
+    
+    // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+    iframe.onload = function() {
+        const conteudo = iframe.contentDocument.documentElement;
+        const alturaConteudo = conteudo.scrollHeight;
+
+        // Defina a altura do <iframe> para a altura do conteúdo
+        iframe.style.height = alturaConteudo + 'px';
+    };
+});
+
+
+//Definindo o tamanho do paineira-frame
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Obtenha a referência para o elemento <iframe>
+    const iframe = document.getElementById('pracaamaral1941-frame');
+    
+    // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+    iframe.onload = function() {
+        const conteudo = iframe.contentDocument.documentElement;
+        const alturaConteudo = conteudo.scrollHeight;
+
+        // Defina a altura do <iframe> para a altura do conteúdo
+        iframe.style.height = alturaConteudo + 'px';
+    };
+});
+
+
+
+
+//Definindo o tamanho do paineira-frame
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Obtenha a referência para o elemento <iframe>
+    const iframe = document.getElementById('sibiripiruna-frame');
+    
+    // Quando o conteúdo do <iframe> carregar, ajuste o tamanho do <iframe>
+    iframe.onload = function() {
+        const conteudo = iframe.contentDocument.documentElement;
+        const alturaConteudo = conteudo.scrollHeight;
+
+        // Defina a altura do <iframe> para a altura do conteúdo
+        iframe.style.height = alturaConteudo + 'px';
+    };
+});
